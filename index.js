f2afcd97df778267c090f4e193928150fef1d415
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-11-05 01:54:24 +0000

    add license header to main project file

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+/*
+ * index.js
+ *
+ * Release asset downloader
+ *
+ * Copyright (C) 2019 Vilius Sutkus'89
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 const ok = require('@octokit/rest')()
 const fs = require('fs')
